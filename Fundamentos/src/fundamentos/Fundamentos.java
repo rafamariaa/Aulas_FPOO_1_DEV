b1673fffcd69bf1b8f45ccf4570689ca99f4c62c
Rafaella Maria Favero da Cruz <rafaella.favero2@aluno.senai.br>
2023-07-28 10:58:54 -0300

    Declarando String como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -7,11 +7,13 @@ package fundamentos;
 public class Fundamentos {
 
     public static void main(String[] args) {
-    //Declaração de variavel
+    //Declaração de variaveis de tipos primitivos
     String nome = "Moisés";
     int idade = 40;
     boolean estudando = true;
     double media =5.5;
+    
+    String bairro = new String("Centro");
     }
     
 }
