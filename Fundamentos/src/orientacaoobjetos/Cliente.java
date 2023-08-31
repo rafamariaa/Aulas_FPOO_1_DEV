292ddfcb0ee8ce18c2bc09485a1ce0ca3ba6aac8
rafaella <rafaella.favero2@aluno.senai.br>
2023-08-31 10:21:27 -0300

    Enviando atividade

--- Fundamentos/src/orientacaoobjetos/Cliente.java
+++ Fundamentos/src/orientacaoobjetos/Cliente.java
@@ -1,6 +1,6 @@
 package orientacaoobjetos;
 
-public class Cliente {
+public class Cliente implements AluguelFilme{
     private String nome;
     private String email;
 
@@ -37,7 +37,7 @@ public class Cliente {
      * @return 
      */
     public double calcularlocacao(int qtdeDias){
-        return qtdeDias * 3.99;
+        return qtdeDias * VALOR_FILME_SIMPLES;
     }
     
     /**
@@ -48,6 +48,6 @@ public class Cliente {
      * @return 
      */
     public double calcularlocacao(int qtdeDias, boolean lancamento){
-        return qtdeDias * 6.99;
+        return qtdeDias * VALOR_FILME_LANCAMENTO;
     }
 }
